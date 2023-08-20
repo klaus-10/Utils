3532278f3b0a46cf72ceba3454464498e755d9bd
klaus-10 <k.shala@campus.unimib.it>
2023-08-20 20:34:50 +0200

    choose output filename feature

--- GeojsonParser.js
+++ GeojsonParser.js
@@ -10,39 +10,45 @@ rl.question("Enter the path to the GeoJSON file: ", (geojsonFilePath) => {
   rl.question(
     'Enter additional info as a comma-separated array (e.g., "value1,value2,value3"): ',
     (additionalInfo) => {
-      const additionalInfoArray = additionalInfo.split(",");
+      rl.question(
+        "Enter the desired output filename (without extension): ",
+        (outputFilename) => {
+          const additionalInfoArray = additionalInfo.split(",");
 
-      // Read the GeoJSON file
-      fs.readFile(geojsonFilePath, "utf8", (err, data) => {
-        if (err) {
-          console.error("Error reading the file:", err);
-          rl.close();
-          return;
+          // Read the GeoJSON file
+          fs.readFile(geojsonFilePath, "utf8", (err, data) => {
+            if (err) {
+              console.error("Error reading the file:", err);
+              rl.close();
+              return;
+            }
+
+            // Parse the GeoJSON data
+            const parsedData = JSON.parse(data);
+
+            // Filter out features based on additional info array
+            const clonedData = {
+              ...parsedData,
+              features: parsedData.features.filter((feature) => {
+                return !additionalInfoArray.includes(feature.properties.info);
+              }),
+            };
+
+            // Generate the output file name based on user input
+            const outputFilePath = `${outputFilename}.geojson`;
+
+            // Write out the new GeoJSON file
+            fs.writeFileSync(
+              outputFilePath,
+              JSON.stringify(clonedData, null, 2)
+            );
+
+            console.log(`New GeoJSON file written to ${outputFilePath}`);
+
+            rl.close();
+          });
         }
-
-        // Parse the GeoJSON data
-        const parsedData = JSON.parse(data);
-
-        // Filter out features based on additional info array
-        const clonedData = {
-          ...parsedData,
-          features: parsedData.features.filter((feature) => {
-            return !additionalInfoArray.includes(feature.geometry.type);
-          }),
-        };
-
-        // Generate a unique output file name based on timestamp
-        const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
-        const outputFileName = `output_${timestamp}.geojson`;
-
-        // Write out the new GeoJSON file
-        const outputFilePath = outputFileName;
-        fs.writeFileSync(outputFilePath, JSON.stringify(clonedData, null, 2));
-
-        console.log(`New GeoJSON file written to ${outputFilePath}`);
-
-        rl.close();
-      });
+      );
     }
   );
 });
